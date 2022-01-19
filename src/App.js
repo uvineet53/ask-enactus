ac4b1c52d7857a73dd42c31e9af464f9b60d08fd
khushi-attri <kattri_be21@thapar.edu>
2022-01-19 18:46:07 +0530

    f

--- src/App.js
+++ src/App.js
@@ -2,20 +2,44 @@ import React,{useState} from 'react'
 import './App.css'
 import database from './firebase';
 import { getDatabase, ref, set } from "firebase/database";
+import validator from 'validator';
+import { v4 as uuid } from 'uuid';
 
 function App() {
+  const unique_id = uuid();
+  var validator = require('validator');
   const [text,setText] = useState('');
+  const [emailid,setemailid] = useState('');
+  const [phoneno,setphoneno] = useState('');
   const handleSubmit = async()=> {
-    await set(ref(database, 'questions/'+text), {
-      question:text
+    if(!(validator.isEmail(emailid))){
+      alert("Please enter valid email id");
+      setemailid('');
+    }
+    else if(!(phoneno).match('[0-9]{10}'))  {
+      alert("Please put 10 digit mobile number");
+      setphoneno('');
+    }
+    else if(!text){
+      alert("Please enter your query");
+      setText('');
+    }
+    else{
+      set(ref(database, 'questions/'+unique_id), {
+       emailid,phoneno,question:text
     });
     setText('');
+    setemailid('');
+    setphoneno('');
     alert('Question Submitted Successfully!');
+  }
   }
   return (
     <div>
       <h3>ask<span>Enactus</span></h3>
-      <textarea placeholder="Drop your question here..." rows="5" value={text} onChange={e=>setText(e.target.value)}></textarea>
+      <textarea placeholder="Enter your emailid..." id="emailid"  value={emailid} onChange={e=>setemailid(e.target.value)} ></textarea>
+      <textarea placeholder="Enter your phoneno..." id="phno"  value={phoneno} onChange={e=>setphoneno(e.target.value)}></textarea>
+      <textarea placeholder="Drop your question here..." id="query"  value={text} onChange={e=>setText(e.target.value)}></textarea>
       <button id="submitBtn" onClick={handleSubmit}>Submit</button>
     </div>
   )
